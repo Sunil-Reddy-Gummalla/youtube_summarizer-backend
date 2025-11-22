a9004d512937b204d1060c93b4dd40644eaad9b1
Venkata Sunil Reddy Gummalla <gummalla.sunilreddy@cvnio.com>
2025-11-22 22:24:01 +0400

    added error handling

--- src/handlers/summarize_handler.rs
+++ src/handlers/summarize_handler.rs
@@ -1,22 +1,26 @@
+use crate::{
+    models::summarize_request::SummarizeRequest, services::llm::summarize_text,
+    utils::extract_id::extract_video_id,
+};
+use axum::Json;
 use yt_transcript_rs::YouTubeTranscriptApi;
-use axum::{Json};
-use crate::{models::summarize_request::SummarizeRequest, utils::extract_id::extract_video_id, services::llm::summarize_text};
-
-
 
 pub async fn summarize(Json(req): Json<SummarizeRequest>) -> String {
     let url = req.url;
-    println!("recieved url: {url}");
-    let video_id = extract_video_id(&url) 
-        .expect("Failed to extract video id");
-    println!("video id: {video_id}");
-    let api = YouTubeTranscriptApi::new(None, None, None).expect("failed to get youtube transcript api");
-    let transcript_result = api.fetch_transcript(
-        &video_id,
-        &["en", "es", "en-US"], 
-        false
-    ).await.expect("failed to fetch the transcript");
-    let text = transcript_result.text();
-    let summerized_text = summarize_text(&text).await;
-    summerized_text
+    if let Some(video_id) = extract_video_id(&url) {
+        if let Ok(api) = YouTubeTranscriptApi::new(None, None, None) {
+            if let Ok(transcript_result) = api
+                .fetch_transcript(&video_id, &["en", "es", "en-US"], false)
+                .await {
+                    let text = transcript_result.text();
+                    summarize_text(&text).await
+                } else {
+                    return "Error fetching transcript".to_string();
+                }
+        } else {
+            return "Error fetching transcript".to_string();
+        }
+    } else {
+        "Invalid URL".to_string()
+    }
 }
--- src/services/llm.rs
+++ src/services/llm.rs
@@ -1,26 +1,29 @@
-use async_openai::{
-    Client,
-    config::OpenAIConfig,
-    types::{CreateCompletionRequestArgs},
-};
+use async_openai::{Client, config::OpenAIConfig, types::CreateCompletionRequestArgs};
 use dotenvy::dotenv;
 use std::env;
 
 pub async fn summarize_text(text: &str) -> String {
     dotenv().ok();
 
-    let api_key = env::var("OPENROUTER_API_KEY").unwrap();
-
-    let config = OpenAIConfig::new()
-        .with_api_base("https://openrouter.ai/api/v1")
-        .with_api_key(api_key);
-
-    let client = Client::with_config(config);
-
-    let request = CreateCompletionRequestArgs::default()
-        .model("x-ai/grok-4.1-fast:free")
-        .prompt(format!("Summerize this Youtube Transcript: /n {text}"))
-        .build().unwrap();
-    let response = client.completions().create(request).await.unwrap();
-    response.choices[0].text.clone()
+    if let Ok(api_key) = env::var("OPENROUTER_API_KEY") {
+        let config = OpenAIConfig::new()
+            .with_api_base("https://openrouter.ai/api/v1")
+            .with_api_key(api_key);
+        let client = Client::with_config(config);
+        if let Ok(request) = CreateCompletionRequestArgs::default()
+            .model("x-ai/grok-4.1-fast:free")
+            .prompt(format!("Summerize this Youtube Transcript: /n {text}"))
+            .build()
+        {
+            if let Ok(response) = client.completions().create(request).await {
+                response.choices[0].text.clone()
+            } else {
+                return "Error from response of llm".to_string();
+            }
+        } else {
+            return "Error creating request".to_string();
+        }
+    } else {
+        return "API Key not found".to_string();
+    }
 }
